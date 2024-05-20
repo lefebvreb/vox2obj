755ae7272a5adce3c70bc7a1d64afc198938ce91
Benjamin Lefebvre <benji.lef76@gmail.com>
2024-05-20 14:51:15 +0200

    Scenes

--- src/error.rs
+++ src/error.rs
@@ -5,10 +5,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("the provided .vox file does not contain any model")]
-    EmptyVox,
-    #[error("the provided .vox file contains multiple models, only one is allowed")]
-    TooManyModels,
+    #[error("invalid scene graph")]
+    InvalidSceneGraph,
     #[error("failed to parse .vox file: {0}")]
     DotVox(&'static str),
     #[error("image error: {0}")]
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 mod error;
-mod obj;
+mod model;
 mod palette;
 
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use clap::Parser;
-use obj::Obj;
+use dot_vox::{DotVoxData, SceneNode};
 use palette::Palette;
 
 use crate::error::{Error, Result};
+use crate::model::Obj;
 
 #[derive(Parser, Debug)]
 #[command(version, author, about)]
@@ -19,14 +20,14 @@ pub struct Args {
     #[arg(
         short,
         long,
-        default_value = "out.obj",
-        help = "Path to the output .obj file name"
+        default_value = "out/",
+        help = "Path to the output directory"
     )]
     output: PathBuf,
     #[arg(
         short = 'p',
-        long = "write-palette-to",
-        help = "Write palette to the given directory"
+        long = "palette",
+        help = "Write palette textures to the given directory"
     )]
     palette: Option<PathBuf>,
 }
@@ -34,20 +35,103 @@ pub struct Args {
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    let input = fs::read(&args.input)?;
-    let vox = dot_vox::load_bytes(input.as_ref()).map_err(Error::DotVox)?;
-
-    let obj = match vox.models.as_slice() {
-        [] => return Err(Error::EmptyVox),
-        [ref model] => Obj::new(model),
-        _ => return Err(Error::TooManyModels),
+    // Parse .vox file.
+    let vox = {
+        let input = fs::read(&args.input)?;
+        dot_vox::load_bytes(input.as_ref()).map_err(Error::DotVox)?
     };
 
-    obj.write(args.output)?;
+    // Create output directory.
+    fs::create_dir_all(&args.output)?;
 
+    // Get root group node.
+    let SceneNode::Group { children, .. } = &vox.scenes[1] else {
+        return Err(Error::InvalidSceneGraph);
+    };
+
+    // Convert children.
+    for &child in children {
+        convert_transform(&vox, &args.output, child)?;
+    }
+
+    // Save palette if requested.
     if let Some(path) = args.palette {
         let palette = Palette::new(&vox.palette, &vox.materials);
         palette.write(path)?;
+    }
+
+    Ok(())
+}
+
+// Scene Graph
+//
+// T : Transform Node
+// G : Group Node
+// S : Shape Node
+//
+//      T
+//      |
+//      G
+//     / \
+//    T   T
+//    |   |
+//    G   S
+//   / \
+//  T   T
+//  |   |
+//  S   S
+fn convert_transform(vox: &DotVoxData, path: &Path, scene: u32) -> Result<()> {
+    let SceneNode::Transform {
+        attributes, child, ..
+    } = &vox.scenes[scene as usize]
+    else {
+        return Err(Error::InvalidSceneGraph);
+    };
+
+    let name = attributes.get("_name").cloned();
+
+    match &vox.scenes[*child as usize] {
+        // Group.
+        SceneNode::Group { children, .. } => {
+            let name = name.unwrap_or_else(|| format!("group_{child}"));
+            let path = path.join(name);
+            fs::create_dir_all(&path)?;
+
+            for &child in children {
+                convert_transform(vox, &path, child)?;
+            }
+        }
+        // Shape.
+        SceneNode::Shape { models, .. } => {
+            let name = name.map_or_else(
+                || format!("model_{child}.obj"),
+                |name| format!("{name}.obj"),
+            );
+            let path = path.join(name);
+
+            match models.as_slice() {
+                // Simple model.
+                [model] => {
+                    let obj = Obj::new(&vox.models[model.model_id as usize]);
+                    obj.write(&path)?;
+                }
+                // Animations.
+                frames => {
+                    fs::create_dir_all(&path)?;
+
+                    for frame in frames {
+                        let name = format!(
+                            "frame_{}.obj",
+                            frame.frame_index().ok_or(Error::InvalidSceneGraph)?
+                        );
+                        let obj = Obj::new(&vox.models[frame.model_id as usize]);
+                        obj.write(path.join(name))?;
+                    }
+                }
+            }
+        }
+        // Transforms should not be followed by another Transform.
+        _ => return Err(Error::InvalidSceneGraph),
     }
 
     Ok(())
